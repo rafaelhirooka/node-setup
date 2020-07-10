559b55885fc8f03b476f684b9783aee21f6bbfea
Rafael Hirooka <rafaelhirooka@hotmail.com>
2020-07-10 19:01:07 -0300

    add paths on babel config file

--- babel.config.js
+++ babel.config.js
@@ -14,9 +14,9 @@ module.exports = {
     ['module-resolver', {
       alias: {
         '@config': './src/config',
-        '@models': './src/models',
-        '@controllers': './src/controllers',
-        '@views': './src/views',
+        '@models': './src/app/models',
+        '@controllers': './src/app/controllers',
+        '@services': './src/app/services',
       },
     }],
   ],
